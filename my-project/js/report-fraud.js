c53de14de5f0ac22ae85f3fe749001158f2c077a
kbramh <kylebramhall2@hotmail.com>
2026-02-24 20:53:53 -0800

    fix: page refresh bug

--- my-project/js/report-fraud.js
+++ my-project/js/report-fraud.js
@@ -1,8 +1,16 @@
 const submitButtonValue = document.querySelector('#submitbutton');
-const input = document.querySelector('#submit1'); 
+const input = document.querySelector('#submit1');
 
 function logButtonValue(e) {
-  console.log(input.value); 
+  console.log(input.value);
 }
 
 submitButtonValue.addEventListener('click', logButtonValue);
+
+const formSubmitButtonValue = document.querySelector('#form-submit-button');
+
+function onSubmit(e) {
+  e.preventDefault();
+}
+
+formSubmitButtonValue.addEventListener('submit', onSubmit);
