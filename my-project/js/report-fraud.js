68cd3625a5c52e6408f3c96c5d01171cb3146ec7
kbramh <kylebramhall2@hotmail.com>
2026-02-26 23:57:55 -0800

    feat: added notes

--- my-project/js/report-fraud.js
+++ my-project/js/report-fraud.js
@@ -9,6 +9,8 @@ submitButtonValue.addEventListener('click', logButtonValue);
 
 const formSubmitButtonValue = document.querySelector('#form-submit-button');
 
+//function onSubmit(); fixes a console refresh bug caused by the nature of the form element being used.
+
 function onSubmit(e) {
   e.preventDefault();
 }
