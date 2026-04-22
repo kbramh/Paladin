e78463f71beefd80e916ea1b7f7991e15b753fca
Kyle <131499691+kbramh@users.noreply.github.com>
2026-04-21 17:33:38 -0700

    Report fraud ts conversion (#17)
    * refactor: changed report fraud js file to ts file and fixed a type error
    
    * fix: fixed type error.
    
    * refactor: converted more js code to ts
    
    * refactor: updated the rest of this file to ts

---
+++ src/report-fraud.ts
@@ -0,0 +1,28 @@
+const submitButtonValue = document.querySelector<HTMLButtonElement>("#submitbutton");
+const input = document.querySelector<HTMLInputElement>("#submit1");
+
+function logButtonValue(e: Event) {
+  console.log(input?.value);
+}
+
+if (submitButtonValue) {
+  submitButtonValue.addEventListener("click", logButtonValue);
+}
+
+const formSubmitButtonValue = document.querySelector("#form-submit-button");
+
+//function onSubmit(); fixes a console refresh bug caused by the nature of the form element being used.
+
+function onSubmit(e: Event) {
+  e.preventDefault();
+
+  setTimeout(function () {
+    if (input) input.value = "";
+  });
+}
+
+if (submitButtonValue) {
+  submitButtonValue.addEventListener("submit", onSubmit);
+}
+
+//This file is not being used in our report fraud page at all.
