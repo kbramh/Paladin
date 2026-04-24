f494746746f4f3d363de53d9c05737030f61e9c8
Kyle <131499691+kbramh@users.noreply.github.com>
2026-04-23 21:06:48 -0700

    Extract header code (#19)
    * refactor: consolidate header component from html
    
    * fix: deleted unneeded code

---
+++ src/header.ts
@@ -0,0 +1,30 @@
+const headerContent = `
+    <a href="/public/index.html" class="logoAnchorTag"><div class="logo">Logo</div></a>
+    <nav class="nav-right">
+        <div class="topnav">
+            <ul>
+                <li>
+                    <a href="/public/index.html">Home</a>
+                </li>
+                <li>
+                    <a href="/public/pages/report-fraud.html">Report Fraud</a>
+                </li>
+                <li>
+                    <a href="/public/pages/community-rules.html">Community Rules</a>
+                </li>
+                <li>
+                    <a href="/public/pages/faqs.html">FAQs</a>
+                </li>
+                <li>
+                    <a href="/public/pages/contact-us.html">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+        <div class="right-icons">
+            <a href="#">🔍</a>
+        </div>
+    </nav>
+`;
+
+const headerElement = document.querySelector("header")!;
+headerElement.innerHTML = headerContent;
